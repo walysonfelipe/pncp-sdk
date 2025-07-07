9221c71d6a0a603938f580b7b01f5c1bdb3d45fc
Walyson Assis <walysonfelipe25@gmail.com>
2025-07-07 20:11:30 -0300

    Add comprehensive function tests

--- test.js
+++ test.js
@@ -4,6 +4,32 @@ import { createRequire } from 'module';
 const require = createRequire(import.meta.url);
 const cjs = require('./pncpClient.cjs');
 
-assert.strictEqual(typeof esm.Contratacoes.consultarPublicacao, 'function');
-assert.strictEqual(typeof cjs.Contratacoes.consultarPublicacao, 'function');
-console.log('both import and require work');
+const cases = [
+  ['Contratacoes', ['consultarPublicacao', 'consultarProposta', 'consultarAtualizacao']],
+  ['Contratos', ['consultarPublicacao', 'consultarAtualizacao']],
+  ['Atas', ['consultarPeriodo', 'consultarAtualizacao']],
+  ['PCA', ['consultar', 'consultarUsuario', 'consultarAtualizacao']],
+  ['Instrumentos', ['consultarInclusao']],
+];
+
+for (const [group, funcs] of cases) {
+  const esmGroup = esm[group];
+  const cjsGroup = cjs[group];
+  for (const fn of funcs) {
+    assert.strictEqual(typeof esmGroup[fn], 'function');
+    assert.strictEqual(typeof cjsGroup[fn], 'function');
+  }
+}
+
+for (const [group, funcs] of cases) {
+  const esmGroup = esm[group];
+  const cjsGroup = cjs[group];
+  for (const fn of funcs) {
+    const esmRes = await esmGroup[fn]({});
+    const cjsRes = await cjsGroup[fn]({});
+    assert.deepStrictEqual(esmRes, {});
+    assert.deepStrictEqual(cjsRes, {});
+  }
+}
+
+console.log('all functions work via import and require');
